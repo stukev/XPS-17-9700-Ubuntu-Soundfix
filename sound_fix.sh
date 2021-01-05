9adaceb58b01cabb425142f574875c650956c781
stukev <stukevcode@gmail.com>
2021-01-05 12:50:16 +0100

    Fixed path variables

--- sound_fix.sh
+++ sound_fix.sh
@@ -1,12 +1,16 @@
+#get full path
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
 # remove existing intel sof firmware files
 rm /usr/lib/firmware/intel/sof/ -rf || true
 rm /usr/lib/firmware/intel/sof-tplg/ -rf || true
 
 # add new intel sof firmware files
-cp ./data/intel/sof/* /usr/lib/firmware/intel/
+cp "$SCRIPTPATH"/data/intel/sof/* /usr/lib/firmware/intel/ -r
 
 # remove existing alsa ucm2 files
 rm /usr/share/alsa/ucm2/sof-soundwire -rf || true
 
 # add new alsa ucm2 files
-cp ./data/alsa/sof-soundwire/ /usr/share/alsa/ucm2/
+cp "$SCRIPTPATH"/data/alsa/sof-soundwire/ /usr/share/alsa/ucm2/ -r
