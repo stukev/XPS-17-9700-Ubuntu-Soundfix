d050f7133d301bfdc55fd835b84ddd5bd48cb857
stukev <stukevcode@gmail.com>
2021-01-05 13:06:05 +0100

    Fixed path and syntax

--- sound_fix.sh
+++ sound_fix.sh
@@ -1,4 +1,10 @@
-#get full path
+# check permissions
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# get full path
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
 
@@ -7,10 +13,10 @@ rm /usr/lib/firmware/intel/sof/ -rf || true
 rm /usr/lib/firmware/intel/sof-tplg/ -rf || true
 
 # add new intel sof firmware files
-cp "$SCRIPTPATH"/data/intel/sof/* /usr/lib/firmware/intel/ -r
+cp "$SCRIPTPATH/data/intel/*" /usr/lib/firmware/intel/ -r
 
 # remove existing alsa ucm2 files
 rm /usr/share/alsa/ucm2/sof-soundwire -rf || true
 
 # add new alsa ucm2 files
-cp "$SCRIPTPATH"/data/alsa/sof-soundwire/ /usr/share/alsa/ucm2/ -r
+cp "$SCRIPTPATH/data/alsa/sof-soundwire/" /usr/share/alsa/ucm2/ -r
